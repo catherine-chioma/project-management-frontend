b7392dca717c2c8fb2c47ca06c8f01bd710a811f
unknown <chi.ihuaku@gmail.com>
2025-10-03 02:45:16 -0700

    updated services/api.ts to use backend deployed url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,13 @@
+// src/services/api.ts
 import axios from "axios";
 
+// Use the environment variable for the backend URL
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // http://localhost:5000/api
+  baseURL: import.meta.env.VITE_API_URL, // e.g., https://project-management-r80d.onrender.com/api
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 10000, // optional: 10 seconds timeout
 });
 
 export default api;
